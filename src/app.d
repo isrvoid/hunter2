2dd4a9ee32472db374361031647f432f4e956034
Johannes Teichrieb <isrvoid@gmail.com>
2018-11-25 13:21:48 +0100

    Make limitRepetitions() return range

--- src/app.d
+++ src/app.d
@@ -1,27 +1,28 @@
 import std.range;
+import std.algorithm.comparison : equal;
 
 void main()
 {
 }
 
 struct LimitRepetitions(R, size_t maxRep)
-    if (maxRep >= 1)
+if (maxRep >= 1)
 {
     private R _input;
     private size_t _repCount;
 
-	this(R input)
-	{
-		_input = input;
-	}
+    this(R input)
+    {
+        _input = input;
+    }
 
-	@property auto ref front()
-	{
-		return _input.front;
-	}
+    @property auto ref front()
+    {
+        return _input.front;
+    }
 
-	void popFront()
-	{
+    void popFront()
+    {
         assert(!empty);
         auto prev = _input.front;
         _input.popFront();
@@ -32,46 +33,46 @@ struct LimitRepetitions(R, size_t maxRep)
             else if (++_repCount >= maxRep)
                 popFront();
         }
-	}
+    }
 
-	@property bool empty() nothrow
-	{
-		return _input.empty();
-	}
+    @property bool empty() nothrow
+    {
+        return _input.empty();
+    }
 
 }
 
 auto limitRepetitions(size_t maxRep = 1, Range)(Range r)
-	if (isInputRange!Range)
+if (isInputRange!Range)
 {
-	return LimitRepetitions!(Range, maxRep)(r).array;
+    return LimitRepetitions!(Range, maxRep)(r);
 }
 
 @("limitRepetitions empty input") unittest
 {
-	assert("" == "".limitRepetitions());
+    assert(equal("", "".limitRepetitions));
 }
 
 @("repetitions up to max are unaffected") unittest
 {
-	assert("a" == "a".limitRepetitions);
-	assert("aabb" == "aabb".limitRepetitions!2);
+    assert(equal("a", "a".limitRepetitions));
+    assert(equal("aabb", "aabb".limitRepetitions!2));
 }
 
 @("repetitions above max are skipped") unittest
 {
-	assert("a" == "aa".limitRepetitions);
-	assert("aabb" == "aaaabbb".limitRepetitions!2);
+    assert(equal("a", "aa".limitRepetitions));
+    assert(equal("aabb", "aaaabbb".limitRepetitions!2));
 }
 
 @("limitRepetitions misc input") unittest
 {
     enum noRep = "The quick brown fox jumps over the lazy dog.";
-    assert(noRep == noRep.limitRepetitions);
+    assert(equal(noRep, noRep.limitRepetitions));
 
-    assert("start midle" == "ssssssssstart          midddddddddddddle".limitRepetitions);
-    assert("middlee endd" == "middleeeeeeeeeeeeeeeee endddddddddddddddddddddd".limitRepetitions!2);
-    assert("middllleee" == "middlllllllllllllllllllllllllllllleee".limitRepetitions!3);
+    assert(equal("start midle", "ssssssssstart          midddddddddddddle".limitRepetitions));
+    assert(equal("middlee endd", "middleeeeeeeeeeeeeeeee endddddddddddddddddddddd".limitRepetitions!2));
+    assert(equal("middllleee", "middlllllllllllllllllllllllllllllleee".limitRepetitions!3));
 }
 
 struct Node
