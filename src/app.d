5b851ffc2b87298cc442082eeafb54fc4856bb7a
Johannes Teichrieb <isrvoid@gmail.com>
2018-11-30 18:21:57 +0100

    Add merge()

--- src/app.d
+++ src/app.d
@@ -222,14 +222,19 @@ void indexListFile(string name, ref ShovelNode root)
 
 struct Node
 {
-    dchar id;
-    float f;
+    dchar c;
+    float f = 0.0f;
     Node[] child;
+
+    int opCmp(ref const Node other) const pure nothrow @safe
+    {
+        return (c > other.c) - (c < other.c);
+    }
 }
 
-size_t mergeLength(alias cmp = "a < b ? -1 : a > b ? 1 : 0", R1, R2)(R1 a, R2 b) pure @safe
+size_t mergeLength(alias cmp = "(a > b) - (a < b)", R1, R2)(R1 a, R2 b) pure @safe
 if (allSatisfy!(isInputRange, R1, R2))
-in (a.isSorted && b.isSorted, "ranges have to be sorted")
+in (a.isSorted && b.isSorted, "ranges should be sorted")
 {
     import std.functional : binaryFun;
     auto aLength = a.length, bLength = b.length;
@@ -317,4 +322,113 @@ in (a.isSorted && b.isSorted, "ranges have to be sorted")
 @("mergeLength non strictly ordered input") unittest
 {
     assert(11 == mergeLength("abbceee", "aabbbdeeee"));
+}
+
+void merge(ref Node a, Node b) pure nothrow @safe
+in (a.c == b.c, "Merging nodes should have equal chars")
+{
+    a.f += b.f;
+    // common case is merging smaller node into bigger one
+    if (!b.child)
+        return;
+
+    ptrdiff_t ai = a.child.length - 1;
+    ptrdiff_t bi = b.child.length - 1;
+    immutable mergeLength = mergeLength!"a.opCmp(b)"(a.child, b.child);
+    a.child.length = mergeLength;
+    for (ptrdiff_t wi = mergeLength - 1; wi >= 0; --wi)
+    {
+        immutable unindexable = (bi < 0) - (ai < 0);
+        switch (unindexable ? unindexable : a.child[ai].opCmp(b.child[bi]))
+        {
+            case -1:
+                a.child[wi] = b.child[bi--];
+                break;
+            case 1:
+                a.child[wi] = a.child[ai--];
+                break;
+            case 0:
+                a.child[wi] = a.child[ai--];
+                a.child[wi].merge(b.child[bi--]);
+                break;
+            default: assert(0);
+        }
+    }
+}
+
+@("merge init nodes") unittest
+{
+    Node a;
+    a.merge(Node());
+    assert(a == Node());
+}
+
+@("merge adds f") unittest
+{
+    auto a = Node('a', 0.5);
+    a.merge(Node('a', 0));
+    assert(Node('a', 0.5) == a);
+    a.merge(Node('a', 0.5));
+    assert(Node('a', 1) == a);
+}
+
+@("merge leaf f(0) node has no effect") unittest
+{
+    auto expect = Node('a', 42, [Node('a'), Node('b')]);
+    auto node = expect;
+    node.merge(Node('a'));
+    assert(expect == node);
+}
+
+@("merge into leaf f(0) node effectively overwrites it") unittest
+{
+    auto expect = Node('a', 42, [Node('a'), Node('b')]);
+    auto node = Node('a');
+    node.merge(expect);
+    assert(expect == node);
+}
+
+@("merge unequal children") unittest
+{
+    auto expect = Node('x', 0, [Node('a', 1), Node('b', 2)]);
+    auto node = Node('x', 0, [Node('a', 1)]);
+    node.merge(Node('x', 0, [Node('b', 2)]));
+    assert(expect == node);
+}
+
+@("merge children preserves order") unittest
+{
+    auto expect = Node('x', 0, [Node('a', 1), Node('b', 2)]);
+    auto node = Node('x', 0, [Node('b', 2)]);
+    node.merge(Node('x', 0, [Node('a', 1)]));
+    assert(expect == node);
+}
+
+@("merge adds child's f") unittest
+{
+    auto expect = Node('x', 0, [Node('a', 2)]);
+    auto node = Node('x', 0, [Node('a', 1)]);
+    node.merge(node);
+    assert(expect == node);
+}
+
+@("merge depth > 2") unittest
+{
+    auto a = Node('x', 1, [
+            Node('a', 2, [Node('A', 5)]),
+            Node('b', 3, [Node('B', 6, [Node('1', 8)])]),
+            Node('d', 4, [Node('C', 7)])]);
+    auto b = Node('x', 10, [
+            Node('a', 20, [Node('A', 50)]),
+            Node('b', 30, [Node('B', 60, [Node('0', 90), Node('1', 80)])]),
+            Node('c', 35),
+            Node('d', 40, [Node('C', 70)])]);
+    auto expect = Node('x', 11, [
+            Node('a', 22, [Node('A', 55)]),
+            Node('b', 33, [Node('B', 66, [Node('0', 90), Node('1', 88)])]),
+            Node('c', 35),
+            Node('d', 44, [Node('C', 77)])]);
+
+    a.merge(b);
+    assert(expect == a);
 }
